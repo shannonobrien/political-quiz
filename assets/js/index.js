963651cf4eb5f5a0cceb246fd0391ff4a253f5f0
Nolan Lawson <nolan@nolanlawson.com>
2018-06-03 16:31:02 -0700

    calculate total

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,7 @@
 
   function checkQuizValid () {
     quizValid = __quiz.every(_ => _.Answer)
-    $submitButton.setAttribute('disabled', quizValid)
+    $submitButton.disabled = !quizValid
   }
 
   function persistToStorage () {
@@ -32,12 +32,46 @@
         continue
       }
       __quiz[i].Answer = answer
-      $(`.quiz-radio-option-${answer} > input[name="question-${i + 1}"]`).setAttribute('checked', true)
+      $(`.quiz-radio-option-${answer} > input[name="question-${i + 1}"]`).checked = true
     }
   }
 
   function clearStorage () {
     delete localStorage.answers
+  }
+
+  function calculateResult () {
+
+    const getPoints = _ => {
+      switch (_.Answer) {
+        case 'strongly-oppose':
+          return -2 * _.Points
+        case 'oppose':
+          return -1 * _.Points
+        case 'support':
+          return 1 * _.Points
+        case 'strongly-support':
+          return 2 * _.Points
+        case 'neutral':
+        default:
+          return 0
+      }
+    }
+
+    const getSocial = _ => _.Axis === 'Social'
+    const getEconomic = _ => _.Axis === 'Economic'
+
+    const add = (a, b) => a + b
+
+    let socialScore = __quiz.filter(getSocial).map(getPoints).reduce(add, 0)
+    let economicScore = __quiz.filter(getEconomic).map(getPoints).reduce(add, 0)
+    let socialTotal = __quiz.filter(getSocial).length * 2
+    let economicTotal = __quiz.filter(getEconomic).length * 2
+
+    let socialNormalizedScore = socialScore / socialTotal
+    let economicNormalizedScore = economicScore / economicTotal
+
+    alert(`You scored ${socialNormalizedScore} on the social axis and ${economicNormalizedScore} on the economic axis`)
   }
 
   $quizGrid.addEventListener('change', e => {
@@ -52,7 +86,10 @@
     checkQuizValid()
   })
 
+  $submitButton.addEventListener('click', e => {
+    let result = calculateResult()
+  })
+
   restoreFromStorage()
   checkQuizValid()
-
 })()
