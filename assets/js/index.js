4bfbd1a99b35e5396f563124c71a80f7c0387202
Nolan Lawson <nolan@nolanlawson.com>
2018-06-03 16:13:27 -0700

    add localstorage persistence

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,17 +14,45 @@
     $submitButton.setAttribute('disabled', quizValid)
   }
 
+  function persistToStorage () {
+    localStorage.answers = JSON.stringify(__quiz.map(_ => _.Answer))
+  }
+
+  function restoreFromStorage () {
+    if (!localStorage.answers) {
+      return
+    }
+    let answers = JSON.parse(localStorage.answers)
+    if (answers.length !== __quiz.length) { // maybe these questions will change at some point, who knows
+      return
+    }
+    for (let i = 0; i < answers.length; i++) {
+      let answer = answers[i]
+      if (!answer) {
+        continue
+      }
+      __quiz[i].Answer = answer
+      $(`.quiz-radio-option-${answer} > input[name="question-${i + 1}"]`).setAttribute('checked', true)
+    }
+  }
+
+  function clearStorage () {
+    delete localStorage.answers
+  }
+
   $quizGrid.addEventListener('change', e => {
     let { target } = e
     if (target.localName.toLowerCase() !== 'input') {
       return
     }
 
-    let index = parseInt(target.getAttribute('name').replace(/^question-/, ''), 10)
+    let index = parseInt(target.getAttribute('name').replace(/^question-/, ''), 10) - 1
     __quiz[index].Answer = target.getAttribute('value')
+    persistToStorage()
     checkQuizValid()
   })
 
+  restoreFromStorage()
   checkQuizValid()
 
 })()
